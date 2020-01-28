0b2dc806a3b6d9e4154e6949a598e4ee8596667c
Mariorg3 <59649688+Mariorg3@users.noreply.github.com>
2020-01-28 13:31:11 +0100

    version1.1
    soy mario ,segunda parte sel ahorcado

--- ahorcado/ src/ ahorcado/Ahorcado.java
+++ ahorcado/ src/ ahorcado/Ahorcado.java
@@ -29,3 +29,49 @@ public class Ahorcado {
             parcial[i] = '_';
         }
     }
+    public static void mostrarPalabraParcialYFallos(char[] palabraParcial, int fallos) {
+		// Aqui comienza
+		for (int i = 0; i < palabraParcial.length; i++) {
+			System.out.print(palabraParcial[i] + " ");
+		}
+		System.out.println();
+		// Aqui termina
+		System.out.println("Numero Fallos " + fallos);
+	}
+
+	/
+	 * examina si la palabra parcial ha sido completada, retornando un valor
+	 * boolean que confirma o desmiente este evento
+	 * 
+	 * @param parcial
+	 * @return
+	 */
+	public static boolean comprobarPalabraCompletada(char[] parcial) {
+		boolean completada = true;
+		for (int i = 0; i < parcial.length && completada; i++) {
+			if (parcial[i] == '_')
+				completada = false;
+		}
+		return completada;
+	}
+
+	public static int incrementarFallo(int numeroFallos) {
+		// Aqui comienza
+		int nuevosFallos = numeroFallos + 1;
+		// Aqui termina
+		return nuevosFallos;
+	}
+
+	/
+	 * cuenta con la palabra secreta y la letra actual y además con otro vector
+	 * de letras llamado parcial, de igual tamaño que palabra, que comienza
+	 * lleno de '_'. Estos '_' serán sustituidos por las letras que vamos
+	 * adivinando. Retorna este vector de aciertos junto con una respuesta
+	 * booleana que nos dice si la letra actual se encontraba en la palabra
+	 * secreta o no.
+	 * 
+	 * @param palabra
+	 * @param parcial
+	 * @param letra
+	 * @return
+	 */
