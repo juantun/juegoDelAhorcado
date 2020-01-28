a02bf82e30043c1884f4763327cc046d2aec3574
juantun <59649321+juantun@users.noreply.github.com>
2020-01-28 13:33:46 +0100

    version 1.2
    - se agrega lector de palabra secreta
    - se comprueba la palabra

--- ahorcado/ src/ ahorcado/Ahorcado.java
+++ ahorcado/ src/ ahorcado/Ahorcado.java
@@ -75,3 +75,39 @@ public class Ahorcado {
 	 * @param letra
 	 * @return
 	 */
+	public static boolean comprobarLetraPalabra(char[] palabra, char[] parcial, char letra) {
+        boolean existe = false;
+        for (int i = 0; i < parcial.length; i++) {
+            if (parcial[i] == '_' && palabra[i] == letra) {
+                parcial[i] = palabra[i];
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    /**
+     * Nos entrega una palabra introducida por el usuario
+     * 
+     * @return la palabra introducida
+     */
+    public static char[] pedirPalabraSecreta() {
+        // PedirPalabra
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Introduce la palabra secreta");
+        String palabraSecreta = leer.nextLine();
+        char[] cadena= palabraSecreta.toCharArray();
+        for (int i = 0; i < cadena.length; i++) {
+            cadena[i]=Character.toUpperCase(cadena[i]);
+        }
+        return cadena;
+    }
+
+    public static char pedirLetra() {
+        System.out.println("mete una letra");
+        Scanner leer = new Scanner(System.in);
+        char letra = leer.nextLine().charAt(0);
+        letra = Character.toUpperCase(letra);
+        return letra;
+    }
+}
